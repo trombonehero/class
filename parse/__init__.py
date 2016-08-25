58e45f9855a7875921fdb7f0d884e7bfab240605
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-08-25 16:25:16 -0230

    Refactor 'parse' command to 'parse classlist'.
    We are about to add a 'parse transcript' command.

---
+++ parse/__init__.py
@@ -0,0 +1,13 @@
+import importlib
+
+
+def setup_argparse(parser):
+    subparsers = parser.add_subparsers(dest = 'banner_command')
+
+    classlist = subparsers.add_parser('classlist', help = 'summary class list')
+    classlist.add_argument('file', nargs = '+', help = 'summary class list(s)')
+
+
+def run(args, db):
+    command = 'parse.' + args.banner_command
+    importlib.import_module(command).run(args, db)
