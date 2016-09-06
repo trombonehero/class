41ef35fbe38e2fb23204c54d873d2a3ec92cdd53
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-09-06 20:46:18 -0230

    Allow more flexible formatting of class lists.
    Allow the set of items to print about each student to be specified at the
    command line, with the default being what we were printing before.

--- list.py
+++ list.py
@@ -1,4 +1,15 @@
+import sys
+
 sort_keys = [ 'name', 'username', 'id' ]
+
+
+def setup_argparse(parser):
+    parser.add_argument('--sort-by', default = sort_keys[0],
+        help = 'how to sort students (%s)' % ' | '.join(sort_keys))
+
+    parser.add_argument('details', help = 'details to show for each student',
+        nargs = '*', default = [ 'username', 'id', 'name', 'email', 'group' ])
+
 
 def run(args, db):
     from db import Student
@@ -9,8 +20,25 @@ def run(args, db):
         'id': Student.student_id,
     }
 
+    formatters = {
+        'email': lambda s: '%20s' % s.email(),
+        'group': lambda s: 'Group %-2d' % s.group(),
+        'id': lambda s: '%9s' % s.student_id,
+        'name': lambda s: '%-24s' % s.name(),
+        'username': lambda s: '%12s' % s.username,
+    }
+
+    formatters = [ formatters[key] for key in args.details ]
+
     students = Student.select()
 
     for s in students.order_by(sorters[args.sort_by]):
-        print('%12s %9s %-24s %20s   Group %2d' % (
-            s.username, s.student_id, s.name(), s.email(), s.group()))
+        print_details(s, formatters)
+
+
+def print_details(student, formatters):
+    for f in formatters:
+        sys.stdout.write(f(student))
+        sys.stdout.write(' ')
+
+    sys.stdout.write('\n')
