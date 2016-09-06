e6502e0e4c35535931a940e409e386ab83d35a2c
Jonathan Anderson <jonathan.anderson@ieee.org>
2016-09-06 12:32:34 -0230

    Add histogram of "personal best" average.
    In addition to the histogram of the average of all Eng One courses, also
    add a histogram that only takes each student's best per-course result into
    account.

--- plot/eng-one.py
+++ plot/eng-one.py
@@ -7,6 +7,7 @@ def run(args, db):
 
     courses = collections.defaultdict(list)
     students = collections.defaultdict(list)
+    personal_best = collections.defaultdict(dict)
 
     for e in Entry.select().where(Entry.mark is not None):
         if e.mark is None:
@@ -28,23 +29,35 @@ def run(args, db):
                     (e.code == '1050'
                      or e.code == '1051'))
             ):
-            courses[(e.subject, e.code)].append(e.mark)
-            students[e.student.student_id].append(e.mark)
+            course = (e.subject, e.code)
+            courses[course].append(e.mark)
+
+            stud_id = e.student.student_id
+            students[stud_id].append(e.mark)
+
+            pb = personal_best[stud_id]
+            current = pb[course] if course in pb else 0
+            pb[course] = e.mark if e.mark > current else current
 
 
     import matplotlib.pyplot as plt
 
-    dimensions = (3, len(courses))
+    dimensions = (5, len(courses))
 
-    plt.subplots(nrows = 2, ncols = len(courses))
+    plt.subplots(nrows = 5, ncols = len(courses))
     plt.title('Marks in Engineering One courses')
 
     histaxes = plt.subplot2grid(dimensions, (0, 0), colspan = len(courses))
     histaxes.hist([ sum(s) / len(s) for s in students.values() ], bins = 30)
 
+    histaxes = plt.subplot2grid(dimensions, (1, 0), colspan = len(courses))
+    histaxes.hist([
+        sum(s.values()) / len(s.values()) for s in personal_best.values()
+    ], bins = 30)
+
     items = sorted(courses.items(), key = lambda (course,m): course)
     for i, ((subject,course), marks) in enumerate(items):
-        axes = plt.subplot2grid(dimensions, (1, i), rowspan = 2)
+        axes = plt.subplot2grid(dimensions, (2, i), rowspan = 3)
         axes.boxplot(marks)
         axes.set_ylim([ 0, 100 ])
         axes.set_xlabel('%s %s' % (subject, course))
