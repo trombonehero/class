bf9cce9a3304d391e7e20911c4ba6ef4e05b7e0b
Jonathan Anderson <jonathan.anderson@ieee.org>
2018-05-14 19:09:53 -0230

    Add ability to manually add students.
    This partially addresses #2.

---
+++ add.py
@@ -0,0 +1,37 @@
+def setup_argparse(parser):
+    pass
+
+
+def run(args, db):
+    print(args)
+
+    try:
+        while True:
+            add_user(db)
+
+    except EOFError:
+        return
+
+
+def add_user(db):
+    student_id = prompt('Student ID')
+    s = db.Student(student_id=student_id)
+
+    s.username = prompt('Username')
+    s.forename = prompt('Forename')
+    s.surname = prompt('Surname')
+    s.graduate_student = prompt('Graduate student [y/N]').lower() == 'y'
+
+    # Since Student has a custom primary key (the student ID), we need 
+    # to force the use of INSERT on our first call to save()
+    s.save(force_insert=True)
+
+    print('Added %s' % s)
+
+
+def prompt(msg):
+    while True:
+        s = raw_input('%-24s' % (msg + ':'))
+        if len(s) > 0:
+            return s
+
