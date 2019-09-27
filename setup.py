631ca3a14f1f91f666f81993229d182b74b707fa
Jonathan Anderson <jonathan.anderson@mun.ca>
2019-09-27 14:10:35 -0230

    Switch to using find_package() in setup.py.
    This catches all of our Python source files correctly.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='class',
     version='0.1',
-    py_modules=['class'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'click',
     ],
     entry_points='''
         [console_scripts]
-        class=class:main
+        class=class:cli
     ''',
 )
