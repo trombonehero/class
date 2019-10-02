d27a7596878ab0841de6c245a99a27f7503f91b4
Jonathan Anderson <jonathan.anderson@mun.ca>
2019-10-02 16:15:33 -0300

    Add 'banner check' command.
    This command can be used to check for successful login.

--- class/banner/__init__.py
+++ class/banner/__init__.py
@@ -13,17 +13,50 @@ today = datetime.date.today()
 term = ((today.month - 9) % 12) / 4 + 1
 term = '%04d%02d' % (today.year - term / 2, term)
 
+browser = None
+
+
+class urlmapper:
+    def __init__(self, root):
+        self.root = root
+
+    def map(self, relative):
+        return '%s/%s' % (self.root, relative)
+
 
 @click.group('banner')
 @click.option('--banner-root', help='Root URL for all Banner requests',
               default='https://www5.mun.ca/admit', show_default=True)
 @click.option('--ca-bundle', help='path to CA certificate bundle')
-@click.option('--credential-file', help='username/password file (YAML format)',
-              default='credentials.yaml', show_default=True)
+@click.option('--credentials', help='file containing username and password',
+              type=click.File(), default='credentials.yaml', show_default=True)
 @click.option('--term', help='e.g., 201601 for Fall 2016',
               default=term, show_default=True)
-def cli(banner_root, ca_bunde, credential_file, term):
+def cli(banner_root, ca_bundle, credentials, term):
     """Pull data directly from Banner."""
+
+    import yaml
+
+    urls = urlmapper(banner_root)
+    credential = yaml.load(credentials)
+
+    try:
+        global browser
+
+        browser = login(credential, urls, ca_bundle)
+        browser.set_term(term, urls)
+
+    except BaseException as e:
+        import sys
+        sys.stderr.write('Error: %s\n' % e)
+        return
+
+
+@cli.command()
+def check():
+    """Test the connection to Banner."""
+
+    print('Login successful.')
 
 
 #import getpass
@@ -35,8 +68,6 @@ def cli(banner_root, ca_bunde, credential_file, term):
 #import sys
 #import traceback
 #
-#
-#    subparsers = parser.add_subparsers(dest='banner_command')
 #
 #    crn = subparsers.add_parser('crn', help='find course CRN from Banner')
 #    crn.add_argument('subject', help='academic subject (e.g., ENGI)')
@@ -51,118 +82,80 @@ def cli(banner_root, ca_bunde, credential_file, term):
 #                            help="fetch entire class' transcripts")
 #    transcript.add_argument('id', nargs='?',
 #                            help="fetch only this student's transcript")
-#
-#
-#
-#class ParseError(BaseException):
-#    def __init__(self, message, soup, cause=None):
-#        """
-#        message: a user-readable string
-#        soup: the BeautifulSoup object for the complete web page
-#        cause: a (type,value,traceback) triple from sys.exc_info()
-#        """
-#
-#        import codecs
-#        import tempfile
-#
-#        self.message = message
-#        self.soup = soup
-#        self.cause = cause
-#
-#        self.filename = tempfile.NamedTemporaryFile().name
-#        self.file = codecs.open(self.filename, 'w', 'utf-8')
-#
-#        self.file.write(soup.prettify())
-#
-#    def __str__(self):
-#        if self.cause:
-#            traceback.print_exception(*self.cause)
-#
-#        return 'ParseError: %s (see contents of %s)' % (
-#            self.message, self.filename)
-#
-#
-#
-#
-#class urlmapper:
-#    def __init__(self, root):
-#        self.root = root
-#
-#    def map(self, relative):
-#        return '%s/%s' % (self.root, relative)
-#
-#
-#def run(args, db):
-#    urls = urlmapper(args.banner_root)
-#
-#    if os.path.isfile(args.credential_file):
-#        import yaml
-#        credential = yaml.load(open(args.credential_file))
-#
-#    else:
-#        print("No '%s', prompting for user input" % args.credential_file)
-#        credential = {
-#            'username': input('Username: '),
-#            'password': getpass.getpass(),
-#        }
-#
-#    try:
-#        browser = login(credential, urls, args.ca_bundle)
-#        browser.set_term(args.term, urls)
-#
-#    except requests.exceptions.ConnectionError as e:
-#        sys.stderr.write('Error: %s\n' % e)
-#        return
-#
-#    except ValueError as e:
-#        sys.stderr.write('Error: %s\n' % e)
-#        return
-#
-#    command = 'banner.' + args.banner_command
-#    importlib.import_module(command).run(args, browser, db, urls)
-#
-#
-#def login(credential, urls, ca_bundle):
-#    login_prompt = 'twbkwbis.P_WWWLogin'
-#    login_verify = 'twbkwbis.P_ValLogin'
-#
-#    browser = mechanicalsoup.Browser(soup_config={'features': 'html.parser'})
-#    browser.log = logging.getLogger()
-#
-#    browser.log.info('Logging in via %s' % urls.map(login_prompt))
-#    browser.get(urls.map(login_prompt), verify=ca_bundle)
-#    result = browser.post(urls.map(login_verify), {
-#        'sid': credential['username'],
-#        'PIN': credential['password'],
-#    })
-#
-#    if not result.ok:
-#        raise ValueError(result)
-#
-#    import types
-#    browser.ca_bundle = ca_bundle
-#    browser.set_term = types.MethodType(set_term, browser)
-#    browser.urls = urls
-#
-#    return browser
-#
-#
-#def set_term(browser, term, urls):
-#    """
-#    Set the term (e.g., Fall 2016) on the current browser session.
-#
-#    Unfortunately, Banner doesn't accept explicit term arguments, but instead
-#    stores this information in server-side session state. To make it easier to
-#    deal with this design decision, this function manipulates a Mechanical Soup
-#    browser object to send a form response to Banner with the session's term ID.
-#    """
-#
-#    term_set = 'bwlkostm.P_FacStoreTerm2'
-#
-#    browser.log.debug('Setting term to %s @ %s' % (term, urls.map(term_set)))
-#    result = browser.post(browser.urls.map(term_set), {'term': term})
-#
-#    if not result.ok:
-#        raise ValueError(result)
-#
-#    return result
+
+
+class ParseError(BaseException):
+    def __init__(self, message, soup, cause=None):
+        """
+        message: a user-readable string
+        soup: the BeautifulSoup object for the complete web page
+        cause: a (type,value,traceback) triple from sys.exc_info()
+        """
+
+        import codecs
+        import tempfile
+
+        self.message = message
+        self.soup = soup
+        self.cause = cause
+
+        self.filename = tempfile.NamedTemporaryFile().name
+        self.file = codecs.open(self.filename, 'w', 'utf-8')
+
+        self.file.write(soup.prettify())
+
+    def __str__(self):
+        if self.cause:
+            traceback.print_exception(*self.cause)
+
+        return 'ParseError: %s (see contents of %s)' % (
+            self.message, self.filename)
+
+
+def login(credential, urls, ca_bundle):
+    import logging
+    import mechanicalsoup
+
+    login_prompt = 'twbkwbis.P_WWWLogin'
+    login_verify = 'twbkwbis.P_ValLogin'
+
+    browser = mechanicalsoup.Browser(soup_config={'features': 'html.parser'})
+    browser.log = logging.getLogger()
+
+    browser.log.info('Logging in via %s' % urls.map(login_prompt))
+    browser.get(urls.map(login_prompt), verify=ca_bundle)
+    result = browser.post(urls.map(login_verify), {
+        'sid': credential['username'],
+        'PIN': credential['password'],
+    })
+
+    if not result.ok:
+        raise ValueError(result)
+
+    import types
+    browser.ca_bundle = ca_bundle
+    browser.set_term = types.MethodType(set_term, browser)
+    browser.urls = urls
+
+    return browser
+
+
+def set_term(browser, term, urls):
+    """
+    Set the term (e.g., Fall 2016) on the current browser session.
+
+    Unfortunately, Banner doesn't accept explicit term arguments, but instead
+    stores this information in server-side session state. To make it easier to
+    deal with this design decision, this function manipulates a Mechanical Soup
+    browser object to send a form response to Banner with the session's term ID.
+    """
+
+    term_set = 'bwlkostm.P_FacStoreTerm2'
+
+    browser.log.debug('Setting term to %s @ %s' % (term, urls.map(term_set)))
+    result = browser.post(browser.urls.map(term_set), {'term': term})
+
+    if not result.ok:
+        raise ValueError(result)
+
+    return result
